f77c719dbcc10fe385c85212b1eb86cf6acead70
Shubh Patel <63448110+shuubhpatel@users.noreply.github.com>
2023-09-08 16:26:50 -0500

    Add files via upload

---
+++ QuizApp/quiz_data.py
@@ -0,0 +1,28 @@
+quiz_data = [
+    {
+        "question": "Which period is known as the 'Age of Dinosaurs'?",
+        "choices": ["Jurassic Period", "Ice Age", "Stone Age", "Cretaceous Period"],
+        "answer": "Jurassic Period"
+    },
+    {
+        "question": "What is the largest known dinosaur species?",
+        "choices": ["Tyrannosaurus Rex", "Triceratops", "Brachiosaurus", "Velociraptor"],
+        "answer": "Argentinosaurus"
+    },
+    {
+        "question": "What type of dinosaur is known for its distinctive three-horned skull?",
+        "choices": ["Stegosaurus", "Ankylosaurus", "Pachycephalosaurus", "Triceratops"],
+        "answer": "Triceratops"
+    },
+    {
+        "question": "Which dinosaur is believed to be one of the fastest runners?",
+        "choices": ["Diplodocus", "Stegosaurus", "Velociraptor", "Allosaurus"],
+        "answer": "Velociraptor"
+    },
+    {
+        "question": "What is the name of the process by which a dinosaur fossil becomes mineralized?",
+        "choices": ["Calcification", "Petrification", "Fossilization", "Mineralization"],
+        "answer": "Petrification"
+    }
+    # Add more dinosaur-related questions here
+]
